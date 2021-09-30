ae6d0ddeeac05657bf24ddf484397a9dbf3ad817
jhillgithub <jeremy.hill.email@gmail.com>
2021-09-30 14:00:12 -0500

    fixes global type

--- webviews/src/global.d.ts
+++ webviews/src/global.d.ts
@@ -2,12 +2,10 @@
 import * as _vscode from "vscode";
 
 declare global {
-  const vscode: {
+  const tsvscode: {
     postMessage: ({
       type: string,
-      value: any,
-      text: string,
-      dataType: string,
+      value: any
     }) => void;
     getState: () => any;
     setState: (state: any) => void;
